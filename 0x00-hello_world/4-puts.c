ec38d6ad86f26c75ffff404dc4a0a30f7c0d6b1b
CardinalCandle <youssef-kj@outlook.fr>
2023-03-16 04:38:58 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+
 /**
 * Outputs a character string using puts instead of printf.
 */
+
 int main(void)
 {
+
 	char str[64] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
+
+
 	return (0);
 }
