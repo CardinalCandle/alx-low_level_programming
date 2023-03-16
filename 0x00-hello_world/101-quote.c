e1b6db9bea9ac1bc07895ad8958f403e89a1767e
CardinalCandle <youssef-kj@outlook.fr>
2023-03-16 05:26:41 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* main - Outputs a character string to STDERR using fprintf.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
