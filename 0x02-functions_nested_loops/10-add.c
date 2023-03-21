ace36e0d0463bf2b0d7d6f89bc5d91d5fed75d5c
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 08:45:59 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,11 @@
 #include "main.h"
 /**
 * add - adds two integers and returns the result.
-*
+*@a: integer
+*@b! integer
 * Return: sum of integers
 */
-int add (int a, int b)
+int add(int a, int b)
 {
 return (a + b);
 }
