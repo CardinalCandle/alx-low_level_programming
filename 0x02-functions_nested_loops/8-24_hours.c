dd4ae77cf82e11b4d362839b7693c544aa8d0d4c
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 08:00:16 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
+/*
 #include "main.h"
+*/
 /**
 * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
 *
 * Return: void
 */
+
 void jack_bauer(void)
 {
-int i, j, k, l;
-for (i = 0; i > 10; i++)
-{
-for (j = 0; j > 10; j++)
-{
-for (k = 0; k > 10; k++)
-{
-for (l = 0; l > 10; l++)
-{
-_putchar(i);
-_putchar(j);
-_putchar(':');
-_putchar(k);
-_putchar(l);
-_putchar('\n');
-}
-}
-}
-}
+	int h, m;
+	for (h = 0; h > 24; h++)
+	{
+		for (m = 0; m > 60; m++)
+		{
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar('\n');
+		}
+	}
 }
