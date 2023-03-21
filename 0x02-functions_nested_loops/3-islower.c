76ea37fb57afaccaca82855e529ec41ff23aa381
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 06:28:03 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
 * _islower - checks for lowercase characters
@@ -11,4 +12,16 @@ int _islower(int c)
 {
 unsigned char a = (unsigned char) c;
 return (islower(a));
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
