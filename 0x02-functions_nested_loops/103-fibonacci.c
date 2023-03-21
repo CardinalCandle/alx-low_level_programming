e0fb2f4be66f1652bc43ce6079397a729120e0ab
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 11:14:26 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-void even_liber_abbaci(void);
+void even_liber_abbaci (void);
 /**
 * main - entry point
 *
 * Return: 0
 */
-int
-main(void)
+int main (void)
 {
-even_liber_abbaci();
-return (0);
+	even_liber_abbaci ();
+	return (0);
 }
 
 /**
@@ -20,25 +19,22 @@ return (0);
 *
 * Return: void
 */
-void even_liber_abbaci(void)
+void even_liber_abbaci (void)
 {
-const int max = 4000000;
-long i, n1, n2, fib, sum;
-n2 = 2;
-n1 = 1;
-sum = n2;
-while (fib < max)
-{
-for (i = 2; i < max ; i++)
+	const int max = 4000000;
+	long i, n1, n2, fib, sum;
+	n2 = 2;
+	n1 = 1;
+	sum = n2;
+	for (i = 2; i < max; i++)
 	{
-	fib = n2 + n1;
-	n1 = n2;
-	n2 = fib;
-	if (fib % 2 == 0)
-	{
-	sum += fib;
+		fib = n2 + n1;
+		n1 = n2;
+		n2 = fib;
+		if (fib % 2 == 0 && fib < max && fib > 0)
+		{
+			sum += fib;
+		}
 	}
-	}
-}
-printf("%li\n", sum);
+	printf ("%li\n", sum);
 }
