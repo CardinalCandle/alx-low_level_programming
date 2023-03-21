44a4a97984e272f028f404e57cbeb2c0b6befe6e
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 14:41:03 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,56 +1,33 @@
 #include <stdio.h>
 
-void fibonacci(void);
 /**
-* main - entry point
-*
-* Return: 0
-*/
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
-	fibonacci();
-	return (0);
-}
-
-/**
-* fibonacci - prints the first 50 Fibonacci numbers,
-* starting with 1 and 2, followed by a new line.
-*
-* Return: void
-*/
-void fibonacci(void)
+unsigned long int i, j, k, j1, j2, k1, k2, j = 1, k = 2;
+printf("%lu", j);
+for (i = 1; i < 91; i++)
 {
-	long double c1 = 8362114348;
-	long double c2 = 9848422977;
-	long double d1 = 1353018523;
-	long double d2 = 44706746049;
-	long double e1 = 21892299583;
-	long double e2 = 4555169026;
-	const unsigned long max = 95;
-	long double i, n1, n2, fib;
-
-	n2 = 2;
-	n1 = 1;
-	printf("\n%.0Lf, %.0Lf, ", n1, n2);
-	for (i = 2; i < max; i++)
-	{
-		fib = n2 + n1;
-		if (i == max - 1)
-		{
-			printf("%.0Lf", fib);
-		}
-		else if (i == max - 2 || i == max - 3)
-		{
-			printf("%.0Lf, ", fib + 1);
-		}
-		else
-		{
-			printf("%.0Lf, ", fib);
-		}
-		n1 = n2;
-		n2 = fib;
-		/*printf("\nFibonacci number %lu is%lu", i, fib);*/
-	}
-	printf(", %.0Lf%.0Lf, %.0Lf%.0Lf, %.0Lf%.0Lf", c1, c2, d1, d2, e1, e2);
-	putchar('\n');
+printf(", %lu", k);
+k = k + j;
+j = k - j;
+}
+j1 = j / 1000000000;
+j2 = j % 1000000000;
+k1 = k / 1000000000;
+k2 = k % 1000000000;
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu%lu", k1 + (k2 / 1000000000), k2 % 1000000000);
+k1 = k1 + j1;
+j1 = k1 - j1;
+k2 = k2 + j2;
+j2 = k2 - j2;
+}
+printf("\n");
+return (0);
 }
