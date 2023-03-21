8c2c0ed6e46c41dcd02ba323c8104299226586d2
CardinalCandle <youssef-kj@outlook.fr>
2023-03-21 07:16:57 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - outputs the absolute value of an integer
+* @n: number to check
+* Return: value of the last digit
+*/
+int print_last_digit(int n)
+{
+return (n % 10);
+}
