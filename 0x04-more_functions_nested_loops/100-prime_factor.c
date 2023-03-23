96eaf9d263f1163f9424a24733ba9a863f3d38ea
CardinalCandle <youssef-kj@outlook.fr>
2023-03-23 09:42:47 -0700

    Task 12

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-unsigned long n = 612852475143, i;
+	unsigned long n = 612852475143, i;
 
-for (i = 2 ; i < n ; i++)
-{
-while (n % i == 0)
-{
-n = n / i;
-}
-}
-printf("%lu\n", n);
-return (0);
+	for (i = 2 ; i < n ; i++)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,8 +10,8 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
+		return;
 	}
 	for (i = 0; i < n; i++)
 	{
