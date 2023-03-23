19daf7a2e3ee3efeea27099bf1866e65e4d43869
CardinalCandle <youssef-kj@outlook.fr>
2023-03-23 06:04:28 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+* _isupper - similar ro isupper function in std library
+* @c: integer
+* Return: 1 if true, 0 otherwise
+*/
+bool _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+else
+{
+return (0);
+}
