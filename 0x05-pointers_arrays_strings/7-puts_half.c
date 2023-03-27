12684fc082c00abd858c940c702b65a47fb92776
CardinalCandle <youssef-kj@outlook.fr>
2023-03-27 08:20:34 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,33 @@
 #include "main.h"
 #include <string.h>
-#include <math.h>
 
 /**
- * puts_half - prints last half of a string
- * starting with the first character, followed by a new line.
- * @str: string to print from
- */
+* puts_half - prints last half of a string
+* starting with the first character, followed by a new line.
+* @str: string to print from
+*/
 void puts_half(char *str)
-{
-	int len = strlen(str);
-	int middle = (int) ceil(len / 2.0);
-	int i = middle;
 
-	for (; i < len; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+{
+
+int len = strlen(str);
+int middle = len / 2;
+
+if (len % 2 == 1)
+
+{
+
+middle += 1;
+
+}
+
+for (int i = middle; i < len; i++)
+
+{
+
+_putchar(str[i]);
+
+}
+_putchar('\n');
+
 }
