4274bb1772f7beb0a65b7a15ad1b548aa647ce21
CardinalCandle <youssef-kj@outlook.fr>
2023-03-29 08:17:03 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i;
+	for (i = 0; (int) i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; (int) i < n; i++)
+		dest[i] = '\0';
+	return dest;
+}
