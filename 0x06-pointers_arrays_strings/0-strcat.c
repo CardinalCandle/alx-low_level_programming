b76d17ae5f2ad187ed4cb2dd2655493d00f779be
CardinalCandle <youssef-kj@outlook.fr>
2023-03-29 07:13:37 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,14 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	char *res = (char*) malloc(100 * sizeof(char));
 	int i = 0, j = 0;
 
 	while(dest[i] != '\0')
+	i++;
+	while(src[j] != '\0')
 	{
-		res[j++] = dest[i++];
+		dest[i++] = src[j++];
 	}
-	i = 0;
-	while(src[i] != '\0')
-	{
-		res[j++] = src[i++];
-	}
-	res[j] = '\0';
-	*dest = *res;
-	return (res);
+	dest[i] = '\0';
+	return (dest);
 }
