6c60075ef6c05d4a6fe1ececc23df420752ef8f1
CardinalCandle <youssef-kj@outlook.fr>
2023-03-29 10:35:40 -0700

    Task 8 (101)

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,20 +8,20 @@
 
 char *rot13(char *s)
 {
-    int i, j;
-    char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; input[j]; j++)
-        {
-            if (s[i] == input[j])
-            {
-                s[i] = output[j];
-                break;
-            }
-        }
-    }
-    return (s);
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; input[j]; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
