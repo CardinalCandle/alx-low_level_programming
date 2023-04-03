907450aa9ea6bf671bf005b4618a43c515121e12
CardinalCandle <youssef-kj@outlook.fr>
2023-04-03 14:42:18 +0000

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, res1 = 0, res2 = 0;
+
+	for (i = 0; i < size * size; i += size + 1)
+	{
+		res1 += a[i];
+	}
+	for (i = size -1; i < size * size - 1; i += size - 1)
+	{
+		res2 += a[i];
+	}
+	printf("%i, %i\n", res1, res2);
+}
