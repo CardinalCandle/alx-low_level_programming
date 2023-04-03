8f53e201e66965257cc3224a54a9284e2fa37a24
CardinalCandle <youssef-kj@outlook.fr>
2023-04-03 05:56:35 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,39 +5,26 @@
 * @accept: string
 * Return: unsigned int
 */
-/**unsigned int _strspn(char *s, char *accept)
-{
-	int i, j, count = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			count += (accept[i] == s[j] ? 1 : 0);
-		}
-	}
-	count += (accept[i] == s[j] ? 1 : 0);
-	return (count);
-}
-*/
 unsigned int _strspn(char *s, char *accept)
 {
-    const char *p;
-    const char *a;
-    unsigned int count = 0;
+	const char *p;
+	const char *a;
+	unsigned int count = 0;
 
-    for (p = s; *p != '\0'; p++) {
-        for (a = accept; *a != '\0'; a++) {
-            if (*p == *a) {
-                count++;
-                break;
-            }
-        }
-        if (*a == '\0') {
-            break;
-        }
-    }
-
-    return count;
+	for (p = s; *p != '\0'; p++)
+	{
+		for (a = accept; *a != '\0'; a++)
+		{
+			if (*p == *a)
+			{
+			count++;
+			break;
+			}
+		}
+		if (*a == '\0')
+		{
+		break;
+		}
+	}
+	return (count);
 }
-
