f676551ed9086f8a0a437a233cf59b3a8774ca01
CardinalCandle <youssef-kj@outlook.fr>
2023-04-03 06:28:10 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (; *s != '\0'; s++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+}
