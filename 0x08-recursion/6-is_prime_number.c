5241eab04216c958c0e7b7d39fd16c78607aed58
CardinalCandle <youssef-kj@outlook.fr>
2023-04-06 01:00:29 +0000

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
  */
 int is_prime_number(int n)
 {
-    return (prime(n, 2));
+	return (prime(n, 2));
 }
 
 /**
@@ -20,12 +20,18 @@ int is_prime_number(int n)
  */
 int prime(int n, int i)
 {
-    if (n <= 1)
-        return (0);
-    if (i * i > n)
-        return (1);
-    if (n % i == 0)
-        return (0);
-    return (prime(n, i + 1));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (prime(n, i + 1));
 }
 
