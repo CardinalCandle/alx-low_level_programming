f440dc397e96f9983bbb63afdda72aebda04c4d7
CardinalCandle <youssef-kj@outlook.fr>
2023-04-11 16:30:29 +0000

    Task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,16 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size);
-	while (i < size)
-		array[i++] = c;
+	if (array)
+	{
+		while (i < size)
+		{
+			array[i++] = c;
+		}
 	return (array);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
