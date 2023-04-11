3fa9a9ff07561e914f15f56b65a3d7046e40dc54
CardinalCandle <youssef-kj@outlook.fr>
2023-04-11 19:26:20 +0000

    Task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,6 +8,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i, j;
 	char *cat;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	cat = malloc(sizeof(char) * (len1 + len2 + 1));
