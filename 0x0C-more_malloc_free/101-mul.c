080718fab58360149f3bd7c4b081796081a3f3bf
CardinalCandle <youssef-kj@outlook.fr>
2023-04-14 00:53:32 +0000

    Task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,212 @@
 #include "main.h"
 
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
 /**
-* Error - do thing
-* Return: nuthin
-*/
-void Error(void)
+
+
+
+   * is_digit - checks if a string contains a non-digit char
+
+
+
+    * @s: string to be evaluated
+
+
+
+     *
+
+
+
+      * Return: 0 if a non-digit is found, 1 otherwise
+
+
+
+       */
+
+int is_digit(char *s)
+
 {
-	printf("Error");
-	exit(98);
+
+  int i = 0;
+
+  while (s[i])
+
+  {
+
+    if (s[i] < '0' || s[i] > '9')
+
+      return (0);
+
+    i++;
+  }
+
+  return (1);
 }
 
 /**
-* is_digit - do thing
-* @grid: arr
-* Return: 1 or 0
-*/
-int is_digit(char **grid)
-{
-	int i, j;
 
-	for (i = 1; i <= 2; i++)
-		for (j = 0; grid[i][j]; j++)
-		{
-			if (grid[i][j] < '0' || grid[i][j] > '9')
-				return (0);
-		}
-	return (1);
+
+
+   * _strlen - returns the length of a string
+
+
+
+    * @s: string to evaluate
+
+
+
+     *
+
+
+
+      * Return: the length of the string
+
+
+
+       */
+
+int _strlen(char *s)
+
+{
+
+  int i = 0;
+
+  while (s[i] != '\0')
+
+  {
+
+    i++;
+  }
+
+  return (i);
 }
 
 /**
-* _calloc - do thing
-* @nmemb: int
-* @size: int
-* Return: ptr
-*/
-void *_calloc(unsigned int nmemb, unsigned int size)
+
+
+
+   * errors - handles errors for main
+
+
+
+    */
+
+void errors(void)
+
 {
-	unsigned int i;
-	char *a;
 
-	if (nmemb == 0 || size == 0)
-		return (0);
+  printf("Error\n");
 
-	a = malloc(nmemb * size);
-	if (!a)
-		return (0);
-
-	for (i = 0; i < (nmemb * size); i++)
-		a[i] = 0;
-
-	return (a);
+  exit(98);
 }
 
 /**
-* main - main entry
-* @argc: int
-* @argv: chars
-* Return: always 0 (Success)
-*/
-int main(int argc, char **argv)
+
+
+
+   * main - multiplies two positive numbers
+
+
+
+    * @argc: number of arguments
+
+
+
+     * @argv: array of arguments
+
+
+
+      *
+
+
+
+       * Return: always 0 (Success)
+
+
+
+        */
+
+int main(int argc, char *argv[])
+
 {
-	int i, j, c, l1 = 0, l2 = 0;
-	char *s1 = argv[1], *s2 = argv[2];
-	int *a, *b, *p;
-	int size = sizeof(int);
 
-	if (argc != 3 || is_digit(argv) != 1)
-		Error();
-	if (*s1 == '0' || *s2 == '0')
-		putchar('0');
-	while (*(argv[1] + l1))
-		l1++;
-	while (*(argv[2] + l2))
-		l2++;
-	a = malloc(l1 * size);
-	b = malloc(l2 * size);
-	p = _calloc(l1 + l2 + 1, size);
-	if (!a || !b || !p)
-		Error();
-	for (i = l1 - 1, j = 0; i >= 0; i--, j++)
-		a[j] = s1[i] - '0';
-	for (i = l2 - 1, j = 0; i >= 0; i--, j++)
-		b[j] = s2[i] - '0';
-	for (i = 0; i < l2; i++)
-		for (j = 0; j < l1; j++)
-			p[i + j] += b[i] * a[j];
-	for (i = 0; i < l1 + l2; i++)
-		c = p[i] / 10, p[i] %= 10, p[i + 1] += c;
-	for (i = l1 + l2; i >= 0; i--)
-		if (p[i] > 0)
-			break;
-	for (; i >= 0; i--)
-		putchar(p[i] + '0');
-	putchar('\n');
-	free(a), free(b), free(p);
-	return (0);
+  char *s1, *s2;
+
+  int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+  s1 = argv[1], s2 = argv[2];
+
+  if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+
+    errors();
+
+  len1 = _strlen(s1);
+
+  len2 = _strlen(s2);
+
+  len = len1 + len2 + 1;
+
+  result = malloc(sizeof(int) * len);
+
+  if (!result)
+
+    return (1);
+
+  for (i = 0; i <= len1 + len2; i++)
+
+    result[i] = 0;
+
+  for (len1 = len1 - 1; len1 >= 0; len1--)
+
+  {
+
+    digit1 = s1[len1] - '0';
+
+    carry = 0;
+
+    for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+
+    {
+
+      digit2 = s2[len2] - '0';
+
+      carry += result[len1 + len2 + 1] + (digit1 * digit2);
+
+      result[len1 + len2 + 1] = carry % 10;
+
+      carry /= 10;
+    }
+
+    if (carry > 0)
+
+      result[len1 + len2 + 1] += carry;
+  }
+
+  for (i = 0; i < len - 1; i++)
+
+  {
+
+    if (result[i])
+
+      a = 1;
+
+    if (a)
+
+      _putchar(result[i] + '0');
+  }
+
+  if (!a)
+
+    _putchar('0');
+
+  _putchar('\n');
+
+  free(result);
+
+  return (0);
 }
-
