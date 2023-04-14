8667d9df1597a55a2061f055123442c3f434894e
CardinalCandle <youssef-kj@outlook.fr>
2023-04-14 00:03:31 +0000

    Task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,70 +1,106 @@
 #include "main.h"
 
-int is_digit(char *s)
+/**
+ * print_error - prints error message and exits
+ */
+void print_error(void)
 {
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
+    printf("Error\n");
+    exit(98);
 }
 
-int errors(void)
+/**
+ * is_digit - checks if all characters in a string are digits
+ * @grid: string to check
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit(char **grid)
 {
-	printf("Error\n");
-	return (98);
+    int i, j;
+
+    for (i = 1; i <= 2; i++)
+        for (j = 0; grid[i][j]; j++)
+            if (grid[i][j] < '0' || grid[i][j] > '9')
+                return (0);
+    return (1);
 }
 
-int main(int argc, char *argv[])
+/**
+ * _calloc - allocates memory and initializes it with zero
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i, len, len1, len2, *result;
-	int j, digit1, digit2, carry, start;
+    void *ptr;
 
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		return (errors());
-	len1 = 0;
-	len2 = 0;
-	while (argv[1][len1])
-		len1++;
-	while (argv[2][len2])
-		len2++;
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return (NULL);
 
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		digit1 = argv[1][i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = argv[2][j] - '0';
-			carry += result[i + j + 1] + (digit1 * digit2);
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[i] += carry;
-	}
-
-	start = 0;
-	while (start < len - 1 && result[start] == 0)
-		start++;
-
-	if (start == len - 1)
-		putchar('0');
-	else
-	{
-		for (i = start; i < len; i++)
-			putchar(result[i] + '0');
-	}
-	putchar('\n');
-	free(result);
-	return (0);
+    memset(ptr, 0, nmemb * size);
+    return (ptr);
 }
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+    int i, j, carry, len1 = 0, len2 = 0;
+    char *str1 = argv[1], *str2 = argv[2];
+    int *num1, *num2, *product;
+
+    if (argc != 3 || is_digit(argv) != 1)
+        print_error();
+
+    while (str1[len1])
+        len1++;
+    while (str2[len2])
+        len2++;
+
+    num1 = malloc(len1 * sizeof(int));
+    num2 = malloc(len2 * sizeof(int));
+    product = _calloc(len1 + len2 + 1, sizeof(int));
+    if (num1 == NULL || num2 == NULL || product == NULL)
+        print_error();
+
+    for (i = len1 - 1, j = 0; i >= 0; i--, j++)
+        num1[j] = str1[i] - '0';
+    for (i = len2 - 1, j = 0; i >= 0; i--, j++)
+        num2[j] = str2[i] - '0';
+
+    for (i = 0; i < len2; i++)
+        for (j = 0; j < len1; j++)
+            product[i + j] += num2[i] * num1[j];
+
+    for (i = 0; i < len1 + len2; i++)
+    {
+        carry = product[i] / 10;
+        product[i] %= 10;
+        product[i + 1] += carry;
+    }
+
+    for (i = len1 + len2; i >= 0; i--)
+        if (product[i] > 0)
+            break;
+    if (i == -1)
+        printf("0\n");
+    else
+    {
+        for (; i >= 0; i--)
+            printf("%d", product[i]);
+        printf("\n");
+    }
+
+    free(num1), free(num2), free(product);
+    return (0);
+}
+
