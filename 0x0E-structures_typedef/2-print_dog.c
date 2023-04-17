93cca03c4a602d208c7cb21bfef9bb7e4e035aac
CardinalCandle <youssef-kj@outlook.fr>
2023-04-17 21:49:49 +0000

    Task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,5 +11,4 @@ void print_dog(struct dog *d)
 	(d->name == NULL) ? printf("nil\n") : printf("Name: %s\n", d->name);
 	(d->age < 0) ? printf("nil\n") : printf("Age: %f\n", d->age);
 	(d->owner == NULL) ? printf("nil\n") : printf("Owner: %s\n", d->owner);
-	}
 }
