e6fee534e9a4c17d08672d53b1f932b76a1b3e45
CardinalCandle <youssef-kj@outlook.fr>
2023-04-17 21:33:42 +0000

    Task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,6 +6,6 @@
 void print_dog(struct dog *d)
 {
 	printf("Name: %s\n", d->name);
-        printf("Age: %f\n", d->age);
-        printf("Owner: %s\n", d->owner);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
