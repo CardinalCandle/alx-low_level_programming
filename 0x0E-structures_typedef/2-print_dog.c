620b13b8eca91fb6ec3c2a0736bc396f2937a8e7
CardinalCandle <youssef-kj@outlook.fr>
2023-04-17 21:32:08 +0000

    Task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+/**
+* print_dog - do thing
+* @d: struct
+*/
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+        printf("Name: %f\n", d->age);
+        printf("Name: %s\n", d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stddef.h>
+#include <stdio.h>
 /**
 * struct dog - struct
 * @name: string
@@ -15,4 +16,5 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
