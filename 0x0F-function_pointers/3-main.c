f9b8774f280bd128e1dcb6d50b476940eeebb289
CardinalCandle <youssef-kj@outlook.fr>
2023-04-19 21:29:19 +0000

    Task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,14 @@
 #include "3-calc.h"
 
 /**
- * main - Entry point
- *
- * @argc: The number of arguments passed to the program
- * @argv: An array containing the arguments passed to the program
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * @argc: int
+ * @argv: args
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
-	int result;
+	int a, b, result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
@@ -35,9 +31,10 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	result = op_func(a, b);
 
 	printf("%d\n", result);
-
 	return (0);
 }
