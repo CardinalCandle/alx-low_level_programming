6acffbd5b457021ea2a73b641f90f13f623fe953
CardinalCandle <youssef-kj@outlook.fr>
2023-04-20 13:31:42 +0000

    Task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 	return (0);
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	while (i < n)
 	{
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 		i++;
 	}
 
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 }
