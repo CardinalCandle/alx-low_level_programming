322d0f8d574b0ea2d9df81a9417e070ff4cc7793
CardinalCandle <youssef-kj@outlook.fr>
2023-04-27 17:14:02 +0000

    Task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,12 +6,12 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        while (h != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
-                count++;
-        }
+		count++;
+	}
 	return (count);
 }
